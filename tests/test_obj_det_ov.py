264c56ad7914201968cda105ebc632acf596fd5d
Daisuke Toya <dtoya2020@gmail.com>
2023-11-09 20:47:01 +0900

    Add test script

---
+++ tests/test_obj_det_ov.py
@@ -0,0 +1,42 @@
+import os
+#import open3d.ml as _ml3d
+import ml3d as _ml3d
+#import open3d.ml.torch as ml3d
+import ml3d.torch as ml3d
+from tqdm import tqdm
+
+cfg_file = "ml3d/configs/pointpillars_kitti.yml"
+cfg = _ml3d.utils.Config.load_from_file(cfg_file)
+
+model = ml3d.models.PointPillars(**cfg.model, device='cpu')
+model = ml3d.models.OpenVINOModel(model)
+model.to('cpu')
+
+cfg.dataset['dataset_path'] = "../dataset/KITTI"
+dataset = _ml3d.datasets.KITTI(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
+pipeline = ml3d.pipelines.ObjectDetection(model, dataset=dataset, device="gpu", **cfg.pipeline)
+
+# download the weights.
+ckpt_folder = "./logs/"
+os.makedirs(ckpt_folder, exist_ok=True)
+ckpt_path = ckpt_folder + "pointpillars_kitti_202012221652utc.pth"
+pointpillar_url = "https://storage.googleapis.com/open3d-releases/model-zoo/pointpillars_kitti_202012221652utc.pth"
+if not os.path.exists(ckpt_path):
+    cmd = "wget {} -O {}".format(pointpillar_url, ckpt_path)
+    os.system(cmd)
+
+# load the parameters.
+pipeline.load_ckpt(ckpt_path=ckpt_path)
+
+test_split = dataset.get_split("test")
+data = test_split.get_data(0)
+
+# run inference on a single example.
+# returns dict with 'predict_labels' and 'predict_scores'.
+
+for idx in tqdm(range(100)):
+    data = test_split.get_data(idx)
+    result = pipeline.run_inference(data)
+
+# evaluate performance on the test set; this will write logs to './logs'.
+pipeline.run_test()
